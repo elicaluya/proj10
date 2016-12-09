001e506f12e0d1c5a26211259ffa6425806903af
Elijah Caluya <elijahcaluya@Elijahs-MacBook-Pro.local>
2016-12-09 03:06:05 -0800

    submit project 10

---
+++ CONFIG.py
@@ -0,0 +1,11 @@
+"""
+Configuration of MeetMe app 
+Generated Thu Dec  1 14:07:23 PST 2016
+May be edited to fit target environment
+
+"""
+
+PORT=5000
+DEBUG = True  # Set to False for production use
+secret_key="a5929cfc82b01f7511b14e9be306045f"
+
